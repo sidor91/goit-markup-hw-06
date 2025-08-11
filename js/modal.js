c4c75f4f5a765f6cc63677f94cfd5396bb492cc9
serhiisydorenko-wdl <sidorchik91@gmail.com>
2025-08-11 20:25:42 +0200

    UPDATE | layout, images and styles

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,44 @@
 (() => {
   const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector("[data-modal]"),
+    // керування модальним вікном
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+
+    // керування мобільним меню
+    mobileBtn: document.querySelector('[data-menu-button]'),
+    mobileMenu: document.querySelector('.mobile-menu'),
+    mobileBtnClose: document.querySelector('.mobile-menu-close'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn?.addEventListener('click', toggleModal);
+  refs.closeModalBtn?.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle("is-open");
+    refs.modal.classList.toggle('is-open');
   }
+
+  refs.mobileBtn.addEventListener('click', function () {
+    refs.mobileMenu.classList.add('open');
+    refs.mobileMenu.classList.remove('closing');
+  });
+
+  function closeMenu() {
+    refs.mobileMenu.classList.add('closing');
+    setTimeout(() => {
+      refs.mobileMenu.classList.remove('open', 'closing');
+    }, 300); // Matches the transition duration (0.3s)
+  }
+
+  refs.mobileBtnClose.addEventListener('click', closeMenu);
+
+  // Close menu when clicking outside of it
+  document.addEventListener('click', function (event) {
+    if (
+      !refs.mobileMenu.contains(event.target) &&
+      !refs.mobileBtn.contains(event.target)
+    ) {
+      closeMenu();
+    }
+  });
 })();
